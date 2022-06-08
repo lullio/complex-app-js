a704ca684e1661f0cd1c9e7e974c56ba87a0f5ee
Felipe WSL-W10-PCFELIPE <felipelullio@gmail.com>
2022-06-08 19:07:32 -0300

    render html configuration FILES
    dizer ao express onde encontrar nossos templates html, precisa instalar uma template-engine pra usar, vamos usar ejs.

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const app = express()
+const app = express();
 
-app('/', function(req, res){
-   res.send("welcome to our new app");
+
+// render o arquivo html da pasta views, express option views
+app.set('views', 'views'); // 1ºviews configuration option 2ºnome da pasta 
+// dizer ao express qual template engine/system vamos usar
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+   res.render('home-guest');
 })
 
 app.listen(process.env.PORT || 3000, function(){
