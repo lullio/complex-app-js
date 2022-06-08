330aa06ddb6928206bc26161c3043a6daff1b3bf
Felipe WSL-W10-PCFELIPE <felipelullio@gmail.com>
2022-06-08 20:38:42 -0300

    post request to create user, set up function
    no router.js criar o router pra url do formulário de cadastro da home page

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,8 +4,8 @@ exports.login = function(){
 exports.logout = function(){
    
 }
-exports.register = function(){
-   
+exports.register = function(req, res){
+   res.send("thanks for register");
 }
 
 exports.home = function(req, res){
